56145a1479a52f4d2c7f9e1bbef08ac39244bc6f
Aram Keryan <13392202+akeryan@users.noreply.github.com>
2024-05-22 18:34:41 +0400

    ex01 update

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: akeryan <akeryan@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 17:53:35 by akeryan           #+#    #+#             */
-/*   Updated: 2024/05/22 18:18:16 by akeryan          ###   ########.fr       */
+/*   Updated: 2024/05/22 18:33:45 by akeryan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,14 @@ int main (void)
 {
 	const Cat *i = new Cat();
 	const Cat *j = new Cat();
-	delete i;
 	delete j;
+	i->getBrain()->addIdea("I am so hungry....");
 
-	Cat a, c;
+	Cat a, c, e(*i);
+
+	std::cout << "E_1 Idea: " << e.getBrain()->getIdea() << std::endl;
+	delete i;
+	std::cout << "E_2 Idea: " << e.getBrain()->getIdea() << std::endl;
 	a.getBrain()->addIdea("Mouuusee");
 	a.getBrain()->addIdea("Fiiiish");
 
